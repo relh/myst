a37a3510345c35b0bfb914e2354b5fe508df5b07
Richard Higgins <richard@relh.net>
2024-03-28 15:36:34 -0400

    cleaned up gitignore

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mamba install pytorch torchvision torchaudio pytorch-cuda -c pytorch -c nvidia
+mamba install scipy scikit-image
+mamba install -c conda-forge diffusers transformers ftfy accelerate
+pip install opencv-python
+pip install -U gradio
+pip install pytorch-lightning einops omegaconf
+pip install timm
