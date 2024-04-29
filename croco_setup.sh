ee5c690ebfa2d60729552bdfd0b53a9120e2fce6
Richard Higgins <richard@relh.net>
2024-04-29 17:52:15 -0400

    added setup for croco compilation

---
+++ croco_setup.sh
@@ -0,0 +1,5 @@
+mamba install habitat-sim headless -c conda-forge -c aihabitat
+mamba install pytorch torchvision -c pytorch
+mamba install notebook ipykernel matplotlib
+mamba install ipywidgets widgetsnbextension
+mamba install scikit-learn tqdm quaternion opencv # only for pretraining / habitat data generation
